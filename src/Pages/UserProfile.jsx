4831451157d29f552ae753ebf42cf4b05f459a36
THASLEEMA-KM <thasleemakm5@gmail.com>
2024-08-20 00:32:08 +0530

    Job Portal Front-End

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -6,7 +6,7 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import SERVER_URL from '../Services/serverURL';
 import { updateProfileResponseContext } from '../Contexts/ContextAPI';
-
+import userPic from '../assets/profilePic.png'
 function UserProfile() {
 
     const [show, setShow] = useState(false);
@@ -105,7 +105,6 @@ function UserProfile() {
 
                   } catch (error) {
                     console.log(error);
-                    
                   }
             }
         }
@@ -168,11 +167,11 @@ function UserProfile() {
                 <div className="col-lg-3"></div>
                 <div className="col-lg-6 border rounded-5 shadow d-flex flex-column">
                     <div className='text-center mt-5'>
-                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="user profile pic" width={'150px'} height={'150px'} className='img-fluid rounded-circle'/>
+                        <img src={userPic} alt="user profile pic" width={'150px'} height={'150px'} className='img-fluid rounded-circle'/>
                     </div>
                     <div className='pt-3 mb-4 d-flex row'>
                         <div className="col-lg-3"></div>
-                       <div className='col-lg-6 ' style={{textAlign:'justify'}}>
+                       <div className='col-lg-6 ' style={{textAlign:'justify',width:"auto"}}>
                            <h3> <i className="fa-solid fa-circle-user text-primary me-3"></i> {userDetails.username}</h3>
                            <h3><i className="fa-solid fa-envelope text-info me-3"></i>{userDetails.email}</h3>
                            {/* <h3>{userDetails.password}</h3> */}
