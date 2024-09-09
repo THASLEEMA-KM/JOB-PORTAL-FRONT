1d4afeed4a07c892850698cb0da7be1f04fdb2b6
THASLEEMA-KM <thasleemakm5@gmail.com>
2024-09-09 15:10:21 +0530

    Job Portal Front-End Design Modification

--- src/Pages/AppliedJobs.jsx
+++ src/Pages/AppliedJobs.jsx
@@ -92,59 +92,61 @@ function AppliedJobs() {
   return (
     <>
      <Header insideUserDashboard={true}/>   
-     <div style={{marginTop:"150px",minHeight:"100vh"}}>
+     <div style={{marginTop:"150px",minHeight:"100vh"}} className='container-fluid'>
 
         <h1 className="my-3 text-center">All Applications</h1>
         <p className='text-center'>Total Count :{userAppliedJobs?.length}</p>
-        <div className='row container-fluid mt-5 d-flex'>
+        <div className='row mt-5 d-flex'>
             <div className="col"></div>
-           <div className='col-lg-8'>
-                <table className='table border w-100 shadow'>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Job Name</th>
-                            <th>User Name</th>
-                            <th>Email</th>
-                            <th>Status</th>
-                            <th>CV</th>
-                            {/* <th>action</th> */}
-                            <th><i className="fa-solid fa-ellipsis"></i></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            currentAppliedJobs?.length>0 &&
-                            currentAppliedJobs?.map((items,index)=>(
-                                <tr key={items?.id}>
-                                <td>{index+1}</td>
-                                <td>{items?.title}</td>
-                                <td>{items?.username}</td>
-                                <td>{items?.email}</td>
-                                <td className={getStatusClassName(items?.status)}>{items?.status}</td>
-                                {/* <td className="items.status=='Approved'?'text-success'">{items?.status}</td> */}
-
-                                {/* <td>{items?.resumeFile}</td> */}
-                                <td>
-                                 <a style={{textDecoration:"none"}}  href={items?.resumeFile} download target="_blank" rel="noopener noreferrer">
-            View CV
-          </a>
-                                 </td>
-
-                                {/* <td>edrtfr</td> */}
-                                <td>
-                                {/* <button onClick={()=>handleDeleteAppliedJob(items?._id)} className="btn  text-danger"> <i className="fa-solid fa-trash"></i> </button> */}
-                                <i onClick={()=>handleDeleteAppliedJob(items?._id)} className="fa-solid fa-trash  text-danger"></i> 
-                                </td>
-                                </tr>
-                            ))
-                            // :
-                            // <div>
-                            //     no applied jobs
-                            // </div>
-                            }
-                    </tbody>
-                </table>
+           <div className='col-lg-10'>
+                <div className='table-responsive'>
+                  <table className='table border shadow'>
+                      <thead>
+                          <tr>
+                              <th>#</th>
+                              <th>Job Name</th>
+                              <th>User Name</th>
+                              <th>Email</th>
+                              <th>Status</th>
+                              <th>CV</th>
+                              {/* <th>action</th> */}
+                              <th><i className="fa-solid fa-ellipsis"></i></th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {
+                              currentAppliedJobs?.length>0 &&
+                              currentAppliedJobs?.map((items,index)=>(
+                                  <tr key={items?.id}>
+                                  <td>{index+1}</td>
+                                  <td>{items?.title}</td>
+                                  <td>{items?.username}</td>
+                                  <td>{items?.email}</td>
+                                  <td className={getStatusClassName(items?.status)}>{items?.status}</td>
+                                  {/* <td className="items.status=='Approved'?'text-success'">{items?.status}</td> */}
+  
+                                  {/* <td>{items?.resumeFile}</td> */}
+                                  <td>
+                                   <a style={{textDecoration:"none"}}  href={items?.resumeFile} download target="_blank" rel="noopener noreferrer">
+              View CV
+            </a>
+                                   </td>
+  
+                                  {/* <td>edrtfr</td> */}
+                                  <td>
+                                  {/* <button onClick={()=>handleDeleteAppliedJob(items?._id)} className="btn  text-danger"> <i className="fa-solid fa-trash"></i> </button> */}
+                                  <i onClick={()=>handleDeleteAppliedJob(items?._id)} className="fa-solid fa-trash  text-danger"></i> 
+                                  </td>
+                                  </tr>
+                              ))
+                              // :
+                              // <div>
+                              //     no applied jobs
+                              // </div>
+                              }
+                      </tbody>
+                  </table>
+                </div>
                 {
                             currentAppliedJobs?.length==0 &&
                         <div className='fw-bolder text-center text-danger'>
